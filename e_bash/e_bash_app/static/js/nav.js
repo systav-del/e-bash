a9f0121d4c3e80993ccabbd7c349b29fbd2305e8
amir <amirkoleno@gmail.com>
2025-12-05 19:54:13 +0500

    добавили станицу регистрации

---
+++ e_bash/e_bash_app/static/js/nav.js
@@ -0,0 +1,46 @@
+$('body').prepend(`
+    <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <i class="fa-solid fa-bolt"></i>
+            E-bash
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarWithDropdown" aria-controls="navbarWithDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse show" id="navbarWithDropdown">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">главная, объявления</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">акции, товары</a
+                ++++
+                >
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">корзина и оформление</a>
+            </li>
+
+                </li>
+            <li class="nav-item">
+                <a class="nav-link" href={% url 'auth' %}>авторизация</a
+                ++++
+                >
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown link
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li>
+            </ul>
+        </div>
+    </div>
+    </nav>    
+`);
--- e_bash/e_bash_app/views.py
+++ e_bash/e_bash_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def auth(request):
+    return render(request, 'auth.html')
